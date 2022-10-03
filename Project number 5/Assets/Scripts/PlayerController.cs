e245f3b267445c921cafd6dd45aab6a494e355a1
Ty1er47 <111007514+Ty1er47@users.noreply.github.com>
2022-10-03 11:32:08 -0700

    START

---
+++ Project number 5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float xRange = 8;
+
+    private float _horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * _horizontalInput * moveSpeed * Time.deltaTime);
+        
+        if(transform.position.x > xRange)
+        
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
